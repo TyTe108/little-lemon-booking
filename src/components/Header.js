f2efb7d6855199691b313d583c48dd44c4b382a9
TyTe108 <Tylim@ucsc.edu>
2023-09-14 10:27:24 -0700

    styled the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
 // Header.js
 import React from "react";
-import food1 from "../images/food1.avif";
+import food1 from "../images/food1.png";
 import { Link } from "react-router-dom";
+import styles from "./Header.module.css"; // Import the CSS module
 
 const Header = ({ setShowBooking }) => {
   const handleReserveClick = () => {
@@ -9,16 +10,20 @@ const Header = ({ setShowBooking }) => {
   };
 
   return (
-    <header>
-      <section>
+    <header className={styles.header}>
+      <section className={styles.headerContent}>
         <div>
           <h2>Little Lemon</h2>
           <h3>Chicago</h3>
           <p>We are a cherished Mediterranean eatery, dedicated to blending time-honored recipes with contemporary innovation.</p>
-          <Link to="/booking"><button aria-label="On Click" onClick={handleReserveClick}>Reserve Table</button></Link>
+          <Link to="/booking">
+            <button aria-label="On Click" onClick={handleReserveClick}>
+              Reserve Table
+            </button>
+          </Link>
         </div>
         <div>
-          <img src={food1} alt="Delicious Mediterranean Food" />
+          <img src={food1} alt="Delicious Mediterranean Food" className={styles.foodImage} />
         </div>
       </section>
     </header>
